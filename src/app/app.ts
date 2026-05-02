fd67fe824303429798a34ceda9b9e48e25258cfb
Verónica <vero7nicaxs@gmail.com>
2026-05-02 14:28:59 +0200

    Actualización "app.ts": modificacion anyadirSuma() y resultadoNumero() para evitar NaN

--- src/app/app.ts
+++ src/app/app.ts
@@ -27,11 +27,8 @@ export class App {
       if(this.resultado.length === 0 ){
         // Número 1 es '0', solo faltaría que el resultado tuviera un '0' al principio.
         this.resultado = '0';
-      }else{
-        this.numero1 = parseInt(this.resultado);
       }
       this.resultado = this.resultado + mas;
-      console.log("Numero 1: ", this.numero1,  " --- resultado: ", this.resultado);
     }
     
   }
@@ -45,11 +42,28 @@ export class App {
 
   resultadoNumero(): void {
 
-    // Para obtener el segundo número, se hace un "split" del "+" para quedarme con la segunda parte para numero2.
-    // Ej: 598 + 67 =  ['598', '67'] --> [0]: '598'  ; [1]: '67
-    let resultado_split = this.resultado.split('+');
-    this.numero2 =  parseInt(resultado_split[1]);
-    this.resultado = (this.numero1 + this.numero2).toString();
-    console.log("Resultado: ", this.resultado);
+    // El botón de "=" hará su función si tiene un "+" 
+    if(this.resultado.includes('+')){
+
+      /* Para obtener el segundo número, se hace un "split" del "+" para quedarme con la segunda parte para numero2.
+      Ej: 598 + 67 =  ['598', '67'] --> [0]: '598'  ; [1]: '67
+      */
+      let resultado_split = this.resultado.split('+');
+      console.log("SPLIT: ", resultado_split);
+
+      /* Si después del '+' hay valores, significa que se puede hacer la suma correctamente
+         Ya que si no tiene un "+", no hará nada. */
+      if(resultado_split[1] !== ''){
+        this.numero1 = parseInt(resultado_split[0]);
+        this.numero2 =  parseInt(resultado_split[1]);
+        console.log("N1: ", this.numero1, " + N2: ", this.numero2);
+
+        this.resultado = (this.numero1 + this.numero2).toString();
+        console.log("Resultado: ", this.resultado);
+        // Se restaura el valor de los números.
+        this.numero1 = 0;
+        this.numero2 = 0;
+      }
+    }
   }
 }
